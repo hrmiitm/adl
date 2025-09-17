9757170c930ec46006d2dc45d0141ee18fd8f672
hrmiitm <hrithik999999@gmail.com>
2025-09-17 22:16:42 +0530

    day-3 js string array

--- W0/JS/a.js
+++ W0/JS/a.js
@@ -1,2 +1,13 @@
-var a;
-console.log(a)
+console.log(Array.isArray('Hitesh'))
+console.log(Array.from('Hitesh'))   // Create Array from String
+console.log(Array.from({name: 'hites'}))   // what will happen?
+
+console.log()
+
+console.log(Array.of(100, 200, 300))
+
+console.log()
+const arr = [1,2,3, true, "hi"]
+const arr2 = [7,8,9]
+
+console.log([...arr, ...arr2])
