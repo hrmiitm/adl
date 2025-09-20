0fdd43ed7d79a7bfd7498b2527fb7bd2fbdc2607
hrmiitm <hrithik999999@gmail.com>
2025-09-20 14:43:30 +0530

    python merge sort

--- W1/2_sorting/a.py
+++ W1/2_sorting/a.py
@@ -1,16 +1,38 @@
-arr = [3, 6, 9, 7, 2, 3, 1]
-n = arr.length // 7
+# Merge Sort
 
-for(let i=0; i<n; i++){
+def merge_sort(arr):
+    n = len(arr)
+    if n <= 1: return arr
+
+    # Break
+    larr = arr[:n//2]
+    rarr = arr[n//2:]
+
+    larr = merge_sort(larr)
+    rarr = merge_sort(rarr)
+
+    (n, m) = (len(larr), len(rarr))
+    (i, j) = (0, 0)
+    new_arr = []
+    while(i<n and j<m):
+        if larr[i] < rarr[j]:
+            new_arr.append(larr[i])
+            i += 1
+        else:
+            new_arr.append(rarr[j])
+            j += 1
+    while(i<n):
+        new_arr.append(larr[i])
+        i += 1
+    while(j<m):
+        new_arr.append(rarr[j])
+        j += 1
     
-    let key = arr[i]
-    let j = i-1
+    return new_arr
 
-    while(j>0 && arr[j] > key){
-        arr[j+1] = arr[j]
-        j--
-    }
 
-    arr[j+1] = key
-}
-console.log(arr)
+
+arr = [3, 1, 5, 2, 8, 5, 4]
+
+print(merge_sort(arr))
+
